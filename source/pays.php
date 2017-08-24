bb7a0fa786a577a3f3bcd94cb00a349380e63d5f
polofr <paulhenri.carton@gmail.com>
2017-08-23 21:33:03 -0700

    update countries

--- source/pays.php
+++ source/pays.php
@@ -10,7 +10,7 @@
 </div>
 
 <div class="container py-1">
-  <div class="row text-center">
+  <div class="row text-center"><!-- TODO update this text -->
     <h3 class="mx-auto py-2">Dans chaque hôpital, l’unité d’oncologie pédiatrique dite «Unité-Pilote» a en charge les soins des enfants atteints de cancer</h3>
     <p class="mx-auto text-justify col-md-8">Nous comptons 15 services ou “Unités Pilotes” d’oncologie pédiatrique dans 13 pays et chaque année le nombre d’enfants à prendre en charge augmente. De nouveaux pays cherchent à rejoindre le GFAOP comme le Burundi, le Niger, le Gabon, la Centrafrique qui ont actuellement des pédiatres en formation. On considère que le coût global (hors salaires) du traitement d’un enfant atteint de cancer en Afrique sub-saharienne est d’environ 2000 euros. Ce coût intègre les médicaments anticancéreux pour 1000 euros (antibiotiques, antimitotiques, analyses de sang et petit matériel), la chirurgie pour l’autre moitié.
     Il est à noter que le nombre de cas traités selon les protocoles GFAOP ne représente qu’environ 60 % des enfants accueillis dans les services de cancérologie pédiatrique.
@@ -27,8 +27,12 @@
       <h2><b>ALGERIE</b></h2>
       <p>Adhésion au GFAOP en 2000<br><br>
       <b>Pr Anissa TRABZI</b> (trabzi_anissa@yahoo.fr) <br>
-      - Centre de Beni Messous, Alger<br>
+      <b>Dr Nacira Chérif</b> (naciracherif@hotmail.com) <br>
+      - Centre de Beni Messous, Service de Pédiatrie B, Alger<br>
       - Centre Pierre et Marie Curie, Alger<br>
+      <p>L’unité offre plusieurs centres de prise en charge à Alger et Beni Messous mais il y a aussi d’autres possibilités pour soigner des cancers de l’enfant dans le pays.</p>
+      <p>Le pays : 2 400 000 Km2, 40 millions d’habitants, PIB/hab : 5 484 $, Dépenses de santé/hab : 362 $ (6,6 %). 28,7% <15 ans, 1 200 à 1 300 nouveaux cancers/an chez les <15 ans (1 350 à 1 450 en 2025).*</p>
+      <p>Nombre de cas rapportés par les UP en 2016: 272 traités / 79 sous protocoles GFAOP / 0 enregistré par les data managers</p>
     </div>
   </div>
   <div class="row">
@@ -38,10 +42,16 @@
     <div class="col-12 col-md-8 p-3">
       <h2><b>BURKINA FASO</b></h2>
       <p>Adhésion au GFAOP en 2005<br><br>
+      <b>Pr Olga LOMPO</b> (olgalompo@yahoo.fr)  <br>
+      - CHU Yalgado Ouédraogo, Ouagadougou<br>
       <b>Dr Chantal BOUDA</b> (cgbouda@yahoo.fr)  <br>
       - CHU Yalgado Ouédraogo, Ouagadougou<br>
       <b>Dr Sonia KABORET</b> (kawajo200046@gmail.com) <br>
       - CHU Charles de Gaulle, Ouagadougou<br></p>
+      <p>L’unité est répartie sur 2 centres.</p>
+      <p>Le pays : 274 000 Km2, 18,3 millions d’habitants. PIB/hab : 713 $, Dépenses de santé/hab : 35 $ (4,9 %). Ouagadougou (1,6 million hab). 45,4% <15 ans, 650 à 750 nouveaux cancers/an chez <15 ans (850 à 950 en 2025).*</p>
+      <p>Nombre de cas rapportés par les UP en 2016: 189 traités / 138 sous protocoles GFAOP / 204 enregistrés par les data managers</p>
+      <p>Associations partenaires : ACCIB et Flamme d’Espoir</p>
     </div>
   </div>
   <div class="row cream rounded">
@@ -53,9 +63,26 @@
       <p>Adhésion au GFAOP en 2000<br><br>
       <b>Dr Angèle PONDY</b> (pondyangele@yahoo.fr) <br>
       - Centre Mère et Enfant de la Fondation Chantal Biya, Yaoundé<br></p>
+      <p>Le pays : 475 000 Km2, 23,8 millions d’habitants. PIB/hab:1407$, Dépenses de santé/hab : 59 $ (4,2 %). 42,9% <15 ans, 850 à 950 nouveaux cancers chez <15 ans (1050 à 1 150 en 2025).*</p>
+      <p>Nombre de cas rapportés par les UP en 2016: 124 traités / 57 sous protocoles GFAOP / 124 enregistrés par les data managers</p>
+      <p>Association partenaire : Caritas</p>      
     </div>
   </div>
   <div class="row">
+    <div class="col-12 col-md-4 text-center"><!-- TODO photos -->
+      <img src="images/cameroun.jpg" class="borderless rounded img-thumbnail" width="300px" alt="L'équipe médicale autour de la famille" >
+    </div>
+    <div class="col-12 col-md-8 p-3">
+      <h2><b>CENTRAFRIQUE</b></h2>
+      <p>Adhésion au GFAOP en 2015, fonctionnelle en 2016<br><br><!-- TODO -->
+      <b>Dr Jess Elio Kosh Comba</b> (koshkomba@gmail.com) <br>
+      - Complexe Pédiatrique de Bangui<br></p>
+      <p>Le pays : 623 000 Km2, 5,4 millions d’habitants. PIB/hab : 358 $, Dépenses de santé/hab : 16 $ (4,5 %). Bangui (800 000 habitants). 40,6% <15 ans, 160 à 200 nouveaux cancers chez <15 ans/an, 200 à 260 en 2025).* </p>
+      <p>Nombre de cas rapportés par les UP en 2016: 42 traités / 30 sous protocoles GFAOP / 0 enregistré par les data managers</p>
+      <p>Association partenaire : Bout-Dilat</p>      
+    </div>
+  </div>
+  <div class="row cream rounded">
     <div class="col-12 col-md-4 text-center">
       <img src="images/congo.jpg" class="borderless rounded img-thumbnail" width="300px" alt="réunion dans les locaux au Congo" >
     </div>
@@ -63,8 +90,26 @@
       <h2><b>CONGO RDC</b></h2>
       <p>Adhésion au GFAOP en 2009<br><br>
       <b>Dr Robert LUKAMBA</b> (robert.lukamba@gfaop.org) <br>
-      <b>Dr Gray KATENG</b><br>
       - Cliniques Universitaires de Lubumbashi<br></p>
+      <p>Le pays : 2,345 million Km2, 79,4 millions d’habitants. PIB/hab : 442 $, Dépenses de santé/hab : 12 $ (2,7%). 43,1% <15 ans, 2 500 à 3 000 nouveaux cancers chez <15 ans/an (3 250 à 3 900 en 2025).* </p>
+      <p>Nombre de cas rapportés par les UP en 2016: 44 traités / 25 sous protocoles GFAOP / 47 enregistrés par les data managers</p>
+      <p>Association partenaire : Gabriella Pour la Vue</p>
+      <p>Leur film : https://youtu.be/u9A4gZf7hk8</p>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-12 col-md-4 text-center"><!-- TODO image -->
+      <img src="images/congo.jpg" class="borderless rounded img-thumbnail" width="300px" alt="réunion dans les locaux au Congo" >
+    </div>
+    <div class="col-12 col-md-8 p-3">
+      <h2><b>CONGO KINSHASA</b></h2>
+      <p>Adhésion au GFAOP en 2015, ouverture en 2016<br><br>
+      <b>Pr Jean Lambert Gini</b> (giniehungu@gmail.com) <br>
+      <b>Dr Aleine Budiongo</b> (albudiongo@gmail.com)  <br>            
+      - Cliniques Universitaires de Kinshasa<br></p>
+      <p>Le pays : 2,345 million Km2, 79,4 millions d’habitants. PIB/hab : 442 $, Dépenses de santé/hab : 12 $ (2,7%). 43,1% <15 ans, 2 500 à 3 000 nouveaux cancers chez <15 ans/an (3 250 à 3 900 en 2025).* </p>
+      <p>Nombre de cas rapportés par les UP en 2016: 59 traités / 41 sous protocoles GFAOP / 0 enregistré par les data managers</p>
+      <p>Association partenaire : Les Jeannots</p>      
     </div>
   </div>
   <div class="row cream rounded">
@@ -74,9 +119,25 @@
     <div class="col-12 col-md-8 p-3">
       <h2><b>CÔTE D'IVOIRE</b></h2>
       <p>Adhésion au GFAOP en 2004<br><br>
-      <b>Dr Line COUITCHERE</b> (coutlin2@voila.fr) <br>
-      <b>Dr Jean Jacques ATTEBY</b> (attebyjj@yahoo.fr) <br>
+      <b>Dr Line COUITCHERE</b> (line.couitchere@gmail.com) <br>
+      <b>Dr Jean Jacques ATTEBY YAO</b> (attebyjj@yahoo.fr) <br>
       - CHU de Treichville, Abidjan<br></p>
+      <p>Le pays : 322000Km2 , 23,3 millions d’habitants. PIB/hab:1545$, Dépenses de santé/hab : 88 $ (5,6 %). Abidjan (4,7 million hab). 38,4% <15 ans, 700 à 800 nouveaux cancers chez <15 ans (900 à 1 000 en 2025).*</p>
+      <p>Nombre de cas rapportés par les UP en 2016: 226 traités / 83 sous protocoles GFAOP / 124 enregistrés par les data managers</p>
+      <p>Associations partenaires : Soleterre, AEC</p>   
+      <p>Leur film : https://youtu.be/fRymui0Svd8</p>
+    </div>
+  </div>
+  <div class="row cream rounded">
+    <div class="col-12 col-md-4 text-center">
+      <img src="images/abidjan.png" class="borderless rounded img-thumbnail" width="300px" alt="l'hopital d'Abidjan" >
+    </div>
+    <div class="col-12 col-md-8 p-3">
+      <h2><b>GUINÉE CONAKRY</b></h2>
+      <p>Adhésion au GFAOP en 2013, ouverture en 2017<br><br>
+      <b>Pr Mamadou PATHÉ Diallo</b> (bapathediallo@yahoo.fr) <br>
+      - CHU, Donka<br></p>
+      <p>Le pays : 246 000 Km2 , 11,8 millions d’habitants. PIB/hab : 539 $, dépenses de santé/hab : 30 $ (5,5 %). 42% <15 ans, 375 à 425 nouveaux cancers chez <15 ans/an, 475 à 525 en 2025).* </p>
     </div>
   </div>
   <div class="row">
@@ -87,9 +148,14 @@
       <h2><b>MADAGASCAR</b></h2>
       <p>Adhésion au GFAOP en 2004</p><br>
       <b>Dr Mbola RAKATOMAHOFENA</b> (mahefambola@yahoo.fr) <br>
+      <b>Dr Sylvia Randriamihoatra</b> (rsylv@moov.mg) <br>
+      <b>Dr Joaly Rasolofo</b> (rasolofojoaly@gmail.com) <br>
       <p>- HJRA (Hôpital Joseph Ravoa- hangy Andrianavalona)<br>
       - HJRB (Hôpital Joseph Raseta Befelatanana)<br>
       - HUMET (Hôpital Universitaire Mère Enfant de Tsaralalana)</P>
+      <p>Le pays : 587 000 km2, 23,8 millions d’habitants. PIB/hab : 450 $, Dépenses de santé/hab : 14 $ (3%). Antananarivo (2,2 millions hab). 40,7% <15ans, 750 à 850 nouveaux cancers chez <15 ans/an (950 à 1 050 en 2025).*</p>
+      <p>Nombre de cas rapportés par les UP en 2016: 90 traités / 44 sous protocoles GFAOP / 6 enregistrés par les data managers</p>
+      <p>Association partenaire : ANYMA</p>      
     </div>
   </div>
   <div class="row cream rounded">
@@ -102,6 +168,9 @@
       <b>Pr Boubacar TOGO</b> (togoboubacar2000@yahoo.fr) <br>
       <b>Dr Fousseyni TRAORE</b> (drfousseynitraore@gmail.com) <br>
       - Hôpital Gabriel Touré, Bamako<br></p>
+      <p>Le pays : 1,240 million Km2 , 17 millions d’habitants, Bamako (3 millions hab). PIB/hab : 704 $, Dépenses de santé/hab : 48$(6,8%).47,6%<15ans,600à700nouveauxcancers/ an chez <15 ans (800 à 900 en 2025).*</p>
+      <p>Nombre de cas rapportés par les UP en 2016: 128 traités / 101 sous protocoles GFAOP / 94 enregistrés par les data managers</p>
+      <p>Associations partenaires : AMSCID et AMSEC</p>
     </div>
   </div>
   <div class="row">
@@ -111,8 +180,10 @@
     <div class="col-12 col-md-8 p-3">
       <h2><b>MAURITANIE</b></h2>
       <p>Adhésion au GFAOP en 2011<br><br>
-      <b>Dr Bénina ZEIN</b> (idinifr@yahoo.fr) <br>
+      <b>Dr Bénina ZEIN</b> (ekhtelbenina@gmail.com) <br>
       - Centre Hospitalier National, Nouakchott<br></p>
+      <p>Le pays : 1,026 million Km2 et 3,6 millions d’habitants. PIB/hab : 1 275 $, Dépenses de santé/hab : 49 $ (3,8 %). Nouakchott : 800 000. 39,5% <15 ans, 110 à 130 nouveaux cancers chez <15 ans/an (140 à 170 en 2025).*</p>
+      <p>Nombre de cas rapportés par les UP en 2016: 48 traités / 21 sous protocoles GFAOP / 27 enregistrés par les data managers</p>
     </div>
   </div>
   <div class="row cream">
@@ -134,7 +205,8 @@
       <h4>RABAT</h4><br>
       <b>Pr Mhammed KHATTAB</b> (khattab@menara.ma) <br>
       <b>Dr Laïla HESSISSEN</b> (laila_hsn@yahoo.fr) <br>
-      - Hôpital des enfants de Rabat
+      - Hôpital des enfants de Raba<br>
+      <p>Nombre de cas rapportés par les UP en 2016: 273 traités / 123 sous protocoles GFAOP / 0 enregistré par les data managers</p>
     </div>
   </div>
   <div class="row cream">
@@ -143,10 +215,40 @@
     </div>
     <div class="col-8">
       <h4>MARRAKECH</h4><br>
-      <b>Professeur Jamila ELHOUDZI</b> (ejamila11@yahoo.fr) <br>
-      - CHU Mohammed VI
+      <b>Professeur Jamila ELHOUDZI</b> (jelhoudzi@gmail.com) <br>
+      - CHU Mohammed VI<br>
+      <p>Nombre de cas rapportés par les UP en 2016: 104 traités / 50 sous protocoles GFAOP / 0 enregistré par les data managers</p>
     </div>
   </div>
+  
+  <div class="row cream">
+    <div class="col-12 col-md-4 text-center">
+    <img src="images/marrakech.jpg" class="borderless rounded img-thumbnail" width="300px" alt="les bureaux de l'équipe soignante" >
+    </div>
+    <div class="col-8">
+      <h4>FÈS</h4><br>
+      <b>Dr Sarra Benmiloud</b> (benmiloudsarra@yahoo.fr) <br>
+      -CHU Hassan II<br>
+      <p>Nombre de cas rapportés par les UP en 2016: 125 traités / 66 sous protocoles GFAOP / 0 enregistré par les data managers</p>
+    </div>
+  </div>
+  
+  <div class="row">
+    <div class="col-12 col-md-4 text-center">
+      <img src="images/madagascar.jpg" class="borderless rounded img-thumbnail" width="300px" alt="l'entree de l'hopital" >
+    </div>
+    <div class="col-12 col-md-8 p-3">
+      <h2><b>NIGER</b></h2>
+      <p>Adhésion au GFAOP en 2015, ouverture en 2016<br>
+      <b>Pr ALI MAZOU Hassane</b> (dr_alimazou@yahoo.fr) <br>
+      <b>Dr ADAMOU BOUBACAR Sahadatou</b> (labosahadatou@yahoo.fr) <br>
+      - Hôpital National de Niamey</p>
+      <p>Le pays : 1,267 million Km2 et 18 millions d’habitants. PIB/hab : 427 $, Dépenses de santé/hab : 24 $ (5,6 %). 49,8% <15 ans, 650 à 750 nouveaux cancers chez <15 ans/an, 900 à 1 000 en 2025).*</p>
+      <p>Nombre de cas rapportés par les UP en 2016: 98 traités / 49 sous protocoles GFAOP / 89 enregistrés par les data managers</p>
+      <p>Associations partenaires : APSA, Association pour la santé en Afrique, CONIF</p>  
+      <p>Leur film : https://youtu.be/f5xxR0yytJw</p>          
+    </div>
+  </div>  
   <div class="row">
     <div class="col-12 col-md-4 text-center">
       <img src="images/senegal.jpg" class="borderless rounded img-thumbnail" width="300px" alt="l'équipe soignante au complet" >
@@ -154,9 +256,28 @@
     <div class="col-8">
       <h2><b>SENEGAL</b></h2>
       <p>Adhésion au GFAOP en 2000<br><br>
-      <b>Pr Claude MOREIRA</b> (cmoreira@orange.sn) <br>
+      <b>Pr Claude MOREIRA</b> (drcmoreira@gmail.com) <br>
       <b>Dr Fatou Binétou DIAGNE</b> (fabakonde@gmail.com) <br>
       - Hôpital Aristide le Dantec, Dakar</p>
+      <p>Le pays : 197 000 Km2 et 14 millions d’habitants. PIB/hab : 1 067 $, Dépenses de santé/hab : 50 $ (4,7 %). Dakar (3 millions dans la métropole). 42,5% <15 ans, 550 à 650 nouveaux cas de cancers sont attendus chaque année chez les enfants et adolescents (700 à 800 en 2025).*</p>
+      <p>Nombre de cas rapportés par les UP en 2016: 189 traités / 115 sous protocoles GFAOP / 133 enregistrés par les data managers</p>
+      <p>Associations partenaires : Association En-Vie, Association Lueur d’Espoir, Association Soleterre, Fondation Anne-Marie Dione, Fondation Total Sénégal</p>
+      <p>Leur film réalisé par la Fondation Cognac Jay : https://www.facebook.com/solidarum/videos/1632154573744079/</p>
+    </div>    
+  </div>
+  <div class="row">
+    <div class="col-12 col-md-4 text-center">
+      <img src="images/togo.jpg" class="borderless rounded img-thumbnail" width="300px" alt="l'équipe soignante nous accueillant" >
+    </div>
+    <div class="col-8">
+      <h2><b>TOGO</b></h2>
+      <p>Adhésion au GFAOP en 2009<br>
+      <b>Pr Adama GDABOE</b> (adgbadoe@yahoo.fr) <br>
+      <b>Dr Jules GUÉDÉNON</b> (julesblack@yahoo.fr) <br><br>
+      - CHU Sylvanus Olympio, Lomé</p>
+      <p>Le pays: 56600Km2 et 7,5 millions d’habitants. PIB/hab:635$, Dépenses de santé/hab : 34 $ (5,3 %). Lomé (1 million hab). 40,7% <15 ans, 260 à 340 nouveaux cancers chez <15 ans/an (340 à 440 en 2025).*</p>
+      <p>Nombre de cas rapportés par les UP en 2016: 45 traités / 31 sous protocoles GFAOP / 0 enregistré par les data managers</p>
+      <p>Association partenaire : Victoir</p>
     </div>
   </div>
   <div class="row cream rounded">
@@ -167,43 +288,27 @@
       <h2><b>TUNISIE</b></h2>
       <p>Adhésion au GFAOP en 2000<br><br>
       <b>Pr Siham BARSAOUI</b> (barsaoui.sihem@rns.tn) <br>
+      <b>Pr Fatem Fedhila</b> (faten.fedhila@rns.tn) <br>
       - Hôpital des enfants de Tunis</p>
+      <p>Le pays : 164 000 Km2, 11 millions d’habitants, PIB/hab : 4 420 $, Dépenses de santé/hab : 305 $ (6,9 %). 23% <15 ans, 275 à 350 nouveaux cancers/an chez <15 ans (310 à 390 en 2025).*</p>
+      <p>Nombre de cas rapportés par les UP en 2016: 62 traités / 25 sous protocoles GFAOP / 47 enregistrés par les data managers</p>
+      <p>Association partenaire : Victoir</p>
     </div>
-  </div>
-  <div class="row">
-    <div class="col-12 col-md-4 text-center">
-      <img src="images/togo.jpg" class="borderless rounded img-thumbnail" width="300px" alt="l'équipe soignante nous accueillant" >
-    </div>
-    <div class="col-8">
-      <h2><b>TOGO</b></h2>
-      <p>Adhésion au GFAOP en 2008<br>
-      <b>Pr Adama GDABOE</b> (adgbadoe@yahoo.fr) <br>
-      <b>Dr Jules GUÉDÉNON</b> (julesblack@yahoo.fr) <br><br>
-      - Hôpital Sylvanus Olympio, Lomé</p>
-    </div>
-  </div>
+  </div> 
 </div>
 
 <div class="container py-1">
   <div class="row">
     <div class="col-12 col-md-4 rounded cream m-2 ml-md-auto">
       <h3 class="text-center py-2">Les unités pilotes en formation</h3>
+      <p>Plusieurs installations d’unités spécifiques sont en cours de reconnaissance par le GFAOP. 
+        Elles sont dans des pays ou des régions qui ont déjà envoyé ou vont envoyer un ou plusieurs pédiatres en formation.
+      </p>
       <ul>
-        <li>CONGO BRAZZAVILLE</li>
-      </ul>  
-      <p>Le Dr Ildevert Cyriaque N’Djobo est en cours de formation au Diplôme Universitaire de Cancérologie Pédiatrique à Rabat</p>
-      <ul>
-        <li>GUINEE CONAKRY</li>
-      </ul> 
-    </div>
-    <div class="col-12 col-md-4 rounded cream m-2 mr-md-auto">
-      <h3 class="text-center py-2">Les unités pilotes en préparation</h3>
-      <ul>
-        <li>NIGER </li>
-        <li>CENTRAFRIQUE</li>
-        <li>BURUNDI</li>
-        <li>GABON</li>
-        <li>BENIN</li>
+        <li>Burundi</li>
+        <li>Gabon</li>
+        <li>Bénin</li>
+        <li>Tchad</li>
       </ul>
     </div>
   </div>
