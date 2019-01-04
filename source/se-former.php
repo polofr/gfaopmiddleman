b209d9a7bad4103f26ddd919d3c1dd16bfb736ce
pcarton <paulhenri.carton@gmail.com>
2019-01-04 23:18:59 +0530

    add se-former avec le gfaop

---
+++ source/se-former.php
@@ -0,0 +1,87 @@
+<?php include 'partial/_header.html';?>
+<title>Se former avec le GFAOP</title>
+<meta name="title" content="Se former avec le GFAOP">
+<meta name="description" content="Trouver sa formation pour le cancer pédiatrique avec le GFAOP">
+<meta name="keywords" content="formation, cancer, pédiatrique, offre de formation">
+<?php include 'partial/_navbar.html';?>
+
+<div class="wrapper wrapper-5">
+  <h1 class="py-5">SE FORMER SUR LE CANCER PEDIATRIQUE</h1>
+</div>
+<div class="container">
+  <div class="row text-justify">
+    <div class="mx-auto py-2 col-12 col-md-10">
+      <h5>Vous souhaitez vous former sur le cancer de l'enfant. Découvrez ce que nous vous proposons en fonction de votre situation.
+        Si vous trouvez une formation qui vous intéresse, contactez-nous à <a href="mailto:contact@gfaop.org">contact@gfaop.org</a>
+      </h5>
+    </div>
+  </div>
+  <div id="accordion" class="py-2" role="tablist" aria-multiselectable="true">
+    <div class="card">
+      <div class="card-header cream" role="tab" id="headingOne">
+        <h5 class="mb-0">
+          <a class="text-orange" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+            Je suis médecin en Afrique : chirurgien, ophtalmologiste, radiologue, radiothérapeuthe, anatomopathologiste, onco-pédiatre, etc.
+          </a>
+        </h5>
+      </div>
+      <div id="collapseOne" class="collapse show" role="tabpanel" aria-labelledby="headingOne">
+        <div class="card-block">
+          <ul>
+            <li><a target="_blank" href="formation-ducp.php" >Le Diplôme Universitaire de Cancérologie Pédiatrique (DUCP) au Maroc</a></li>
+            <li><a target="_blank" href="formations.php#diuop-france">Le Diplôme Inter Universitaire d’Oncologie Pédiatrique (DIUOP) en France</a></li>
+            <li><a target="_blank" href="formations.php#formations-collegiales">Les formations collégiales</a></li>
+            <li><a target="_blank" href="formations.php#stages">Les stages en hôpitaux</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="card">
+      <div class="card-header cream" role="tab" id="headingTwo">
+        <h5 class="mb-0">
+          <a class="text-orange collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+            Je suis infirmier, pharmacien ou personel soignant en Afrique
+          </a>
+        </h5>
+      </div>
+      <div id="collapseTwo" class="collapse" role="tabpanel" aria-labelledby="headingTwo">
+        <div class="card-block">
+          <ul>
+            <li><a target="_blank" href="formations.php#formation-infirmiere">La formation infirmière</a></li>
+            <li><a target="_blank" href="formations.php#formation-douleur-et-soins-palliatifs">La formation "Douleur et Soins palliatifs"</a></li>
+            <li><a target="_blank" href="formations.php#formation-diagnostic-precoce">La formation des personnels de santé pour le diagnostic précoce</a></li>
+            <li><a target="_blank" href="formations.php#formations-collegiales">Les formations collégiales</a></li>
+            <li><a target="_blank" href="formations.php#stages">Les stages en hôpitaux</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="card">
+      <div class="card-header cream" role="tab" id="headingThree">
+        <h5 class="mb-0">
+          <a class="text-orange collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+            Je fais déjà partie du GFAOP, je souhaite une formation spécifique au fonctionnement des unités-pilotes
+          </a>
+        </h5>
+      </div>
+      <div id="collapseThree" class="collapse" role="tabpanel" aria-labelledby="headingThree">
+        <div class="card-block">
+          <ul>
+            <li><a target="_blank" href="formations.php#formation-responsable-up">La formation des responsables d’Unités Pilotes</a></li>
+            <li><a target="_blank" href="formations.php#formation-ouverture-up">La formation à l’ouverture des nouvelles unités d’oncologie pédiatrique</a></li>
+            <li><a target="_blank" href="formations.php#formation-data-manager">La formation des Data Managers</a></li>
+            <li><a target="_blank" href="formations.php#formation-up">Les missions de formation dans les Unités Pilotes</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="container">
+  <div class="embed-responsive embed-responsive-16by9 mx-auto">
+    <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/3tSxw5DW-Fw"></iframe>
+  </div>
+</div>
+
+<?php include 'partial/_footer.html';?>
+
